ac1bc000fc72c16ab6e6a8ee9ec8b5b8eae2ddb6
Pascal Küsgen <pascal@pascalmh.de>
2018-01-24 12:48:24 +0100

    chore: do not bump composer.json anymore

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function ( grunt ) {
                 beforeRelease: [], // optional grunt tasks to run after release version is bumped up but before release is packaged
                 afterRelease: [], // optional grunt tasks to run after release is packaged
                 updateVars: [], // optional grunt config objects to update (this will update/set the version property on the object specified)
-                additionalFiles: ['composer.json']
+                additionalFiles: []
             }
         },
 
