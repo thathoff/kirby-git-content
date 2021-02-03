13ed4fb1760b9c81ab80e86517ff3acafddc7bb7
Marc Runkel <marc.runkel@plusforta.de>
2021-02-03 07:01:33 +0100

    Add the ability to disable the plugin via the config.
    There are some environments where I don't want to run the plugin at all.

--- index.php
+++ index.php
@@ -5,19 +5,23 @@
 
 $kirbyGit = new Thathoff\GitContent\KirbyGit();
 
-Kirby::plugin('thathoff/git-content', [
-    'hooks' => $kirbyGit->getHooks(),
-    'routes' => $kirbyGit->getRoutes(),
-    'options' => [
-        'path' => null,
-        'branch' => null,
-        'pull' => null,
-        'push' => null,
-        'commit' => null,
-        'cronHooksEnabled' => null,
-        'commitMessage' => ':action:(:item:): :url:',
-        'windowsMode' => null,
-        'gitBin' => null,
-        'displayErrors' => null,
-    ]
-]);
+# don't load plugin if it's disabled in the config.
+if (!option('thathoff.git-content.disable', false)) {
+    Kirby::plugin('thathoff/git-content', [
+        'hooks'   => $kirbyGit->getHooks(),
+        'routes'  => $kirbyGit->getRoutes(),
+        'options' => [
+            'path'             => null,
+            'branch'           => null,
+            'pull'             => null,
+            'push'             => null,
+            'commit'           => null,
+            'cronHooksEnabled' => null,
+            'commitMessage'    => ':action:(:item:): :url:',
+            'windowsMode'      => null,
+            'gitBin'           => null,
+            'displayErrors'    => null,
+            'disable'          => null,
+        ],
+    ]);
+}
